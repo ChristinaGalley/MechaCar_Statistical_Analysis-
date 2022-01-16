ec991e8a3e1c14b496a287aa7026246bd45fbdd5
C Gall <and04551@umn.edu>
2022-01-16 16:22:21 -0600

    updating challenge and resources

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,3 +9,11 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 Suspension_Coil_table <- read.csv('Suspension_Coil.csv',stringsAsFactors = F) #read in Suspension Coil dataset
 total_summary <- Suspension_Coil_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep') #generate summary statistics
 lot_summary <- Suspension_Coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep') #generate lot_summary statistics
+
+#Deliverable 3
+t.test(Suspension_Coil_table$PSI,mu=1500) #t-test compare difference in PSI across all lots by population mean
+
+#t-test compare difference in PSI for each lot by population mean
+t.test(subset(Suspension_Coil_table$PSI, Suspension_Coil_table$Manufacturing_Lot=="Lot1"),mu=1500)
+t.test(subset(Suspension_Coil_table$PSI, Suspension_Coil_table$Manufacturing_Lot=="Lot2"),mu=1500)
+t.test(subset(Suspension_Coil_table$PSI, Suspension_Coil_table$Manufacturing_Lot=="Lot3"),mu=1500)
