477983c9e6aa6ba01fd1117b7734c49a5adf6960
C Gall <and04551@umn.edu>
2022-01-16 15:47:02 -0600

    updating resources and challenge

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -4,3 +4,8 @@ library(dplyr)
 MechaCar_df <- read.csv('MechaCar_mpg.csv',stringsAsFactors = F) #read in MechaCar dataset
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data = MechaCar_df) #generate multiple linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data = MechaCar_df)) #generate summary statistics
+
+#Deliverable 2
+Suspension_Coil_table <- read.csv('Suspension_Coil.csv',stringsAsFactors = F) #read in Suspension Coil dataset
+total_summary <- Suspension_Coil_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep') #generate summary statistics
+lot_summary <- Suspension_Coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep') #generate lot_summary statistics
